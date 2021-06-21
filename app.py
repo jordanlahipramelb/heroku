10528c349826b09fa3128ba594be4a6d97e5bfbf
jordanlahipramelb <jordan.lahipramelb@gmail.com>
2021-06-21 11:00:18 -0700

    add os for environ vars

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User, Tweet
 from forms import UserForm, TweetForm
-
 from sqlalchemy.exc import IntegrityError
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth_demo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = "abc123"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "hellosecret1")
+print("#########################################")
+print(app.config["SECRET_KEY"])  # this should print hellosecret1 because we didn't set a secret key yet
+print("#########################################")
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 
